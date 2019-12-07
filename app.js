1068dae13868e9e86d9568499b40616c526f1b42
Bilal Shahrour <bilal.shahrour@gmail.com>
2019-12-08 00:56:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+mongoose.connect('mongodb+srv:.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  app.use(bodyParser.json());
+  
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+
+  app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes);
+  
+
+
+      module.exports = app;
